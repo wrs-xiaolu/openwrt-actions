cbec072cce301f4275ff1d3e7fa87f64f3ab1d8b
wrs-xiaolu <56376631+wrs-xiaolu@users.noreply.github.com>
2021-06-24 09:57:33 +0800

    Update custom.sh

--- user/lienol-x64-1907/custom.sh
+++ user/lienol-x64-1907/custom.sh
@@ -3,4 +3,5 @@
 echo "Test custom.sh"
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
-
+git clone https://github.com/kenzok8/openwrt-packages.git         package/openwrt-packages
+git clone https://github.com/kenzok8/small                        package/small
